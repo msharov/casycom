43cfa16599b1afeca7e4c72f34eb2261d2bbd4fb
Mike Sharov <msharov@users.sourceforge.net>
2020-05-22 14:00:25 -0400

    Relicense project under ISC license.

--- test/nfwrk.c
+++ test/nfwrk.c
@@ -1,7 +1,7 @@
 // This file is part of the casycom project
 //
 // Copyright (c) 2015 by Mike Sharov <msharov@users.sourceforge.net>
-// This file is free software, distributed under the MIT License.
+// This file is free software, distributed under the ISC license.
 
 #include "ping.h"
 #include <sys/wait.h>
